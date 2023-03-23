1a61207dbd2116548303b5271424f1783e210d20
CEE-AI <chijiokeihedioha@gmail.com>
2023-03-23 11:49:52 +0100

    first commit

--- contracts/twitter-q1.sol
+++ contracts/twitter-q1.sol
@@ -22,18 +22,33 @@ contract Twitter {
     // ----- END OF DO-NOT-EDIT ----- //
 
     function registerAccount(string calldata _name) external {
-
+        bytes memory bname = bytes(_name);
+        require(bname.length != 0, "Name cannot be an empty string.");
+        User storage newUser = users[msg.sender];
+        newUser.wallet = msg.sender;
+        newUser.name = _name;
     }
 
     function postTweet(string calldata _content) external accountExists(msg.sender) {     
-
+       Tweet memory addTweet = Tweet(nextTweetId, msg.sender, _content, block.timestamp);
+       tweets[nextTweetId] = addTweet;
+       users[msg.sender].userTweets.push(nextTweetId);
+       nextTweetId++;
     }
 
     function readTweets(address _user) view external returns(Tweet[] memory) {
-
+        uint[] memory userTweetIds = users[_user].userTweets;
+        Tweet[] memory userTweets = new Tweet[](userTweetIds.length);
+        for (uint i = 0; i <= userTweetIds.length; i++) {
+            userTweets[i] = tweets[userTweetIds[i]];
+        }
+        return userTweets;
     }
 
     modifier accountExists(address _user) {
+        User memory thisUser = users[_user];
+        bytes memory thisUserBytesStr = bytes(thisUser.name);
+        require(thisUserBytesStr.length != 0, "This wallet does not belong to any account.");
         _;
     }
 
